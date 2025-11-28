c951d501ab685517d9e39fd8b983a41f85c8fcbd
trashisfresh <notmeagain@ukr.net>
2025-11-28 18:17:50 +0000

    update readme with a description

---
+++ main.py
@@ -0,0 +1,4 @@
+def get_book_text(path_to_file, file_contents):
+    file_contents = path_to_file.read()
+    path_to_file = "D:\\1. all work (and no play)\\STUDY\\BookBot project\bookbot\books\frankenstein.txt"
+    return file_contents 
